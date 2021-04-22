00c4568a9ff70601e4a9080038e8e6cc64c05514
wit <what.it.takes.to.business@gmail.com>
2021-04-23 05:31:04 +0900

    package add test

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,5 +7,29 @@ module.exports = {
         modulePath: `../src/cms/cms.js`, // This needs to be relative to the .docz directory
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Rocketseat Gatsby Themes`,
+        short_name: `RS Gatsby Themes`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        display: `standalone`,
+        icon: `static/favicon.png`,
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: `YOUR_ANALYTICS_ID`,
+    //   },
+    // },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://witblog.netlify.app`,
+      },
+    },
   ],
 };
